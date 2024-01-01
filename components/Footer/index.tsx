8fc8259b149bd85a658c3f3bbc6dd5038b8bc854
Sirbinod <00np10@gmail.com>
2024-01-01 23:22:21 +0545

    feat: meme search layout

---
+++ components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const Footer: FC = () => {
+  return (
+    <div className="h-36 bg-slate-300">
+      <div className="container text-center">
+        <span className="text-3xl font-bold">Title</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const Header: FC = () => {
+  return (
+    <header className="h-36 bg-blue-600">
+      <div className="container">
+        <h1 className="text-3xl font-bold">Title</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
